586fd50cfcb348d04117dcb1bd02fab468f810bf
vartikatyagi02 <vartikadhruv@gmail.com>
2019-06-21 15:53:40 +0530

    first commit

---
+++ app/src/main/java/com/example/chatapp/Fragments/APIService.java
@@ -0,0 +1,21 @@
+package com.example.chatapp.Fragments;
+
+import com.example.chatapp.Notifications.MyResponse;
+import com.example.chatapp.Notifications.Sender;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface APIService {
+    @Headers(
+            {
+                    "Content-Type:application/json",
+                    "Authorization:key=HERE YOUR OWN API KEY IS WRITTEN"
+            }
+    )
+
+    @POST("fcm/send")
+    Call<MyResponse> sendNotification(@Body Sender body);
+}
